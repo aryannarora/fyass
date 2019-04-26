ddacd441a0e3c939313217adf34128feaad159bd
aryannarora <aryanarora278@gmail.com>
2019-04-26 15:48:14 +0530

    inti

--- db/init.js
+++ db/init.js
@@ -1,7 +1,7 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.connectionString,
+    connectionString: process.env.DATABASE_URL,
 });
 
 pool.on('error', (err, client) => console.error('Unexpected error on idle client', err));
